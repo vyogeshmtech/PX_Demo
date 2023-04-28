a25aab208faace9f0524ebb5b5f0f637bddfee60
Yogesh Vaishnav <92394635+vyogeshmtech@users.noreply.github.com>
2023-04-28 20:46:26 +0530

    Update login.js

--- login.js
+++ login.js
@@ -16,6 +16,7 @@ function login() {
   
   if (validateUsername(username, text)) {
     console.log("Username is valid");
+    alert("Login successful!");
     window.location.href = "https://vyogeshmtech.github.io/index.html";
   } else {
     console.log("Admin keyword NOT found in username");
